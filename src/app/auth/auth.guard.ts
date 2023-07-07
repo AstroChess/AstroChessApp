89f361f2ad08cebcbd80b82063a23837dfc259cf
xketris <xketris@gmail.com>
2023-07-08 01:24:13 +0200

    auth guard bug fixed

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return this.authService.user.pipe(filter(user=>typeof user !== 'undefined'), map(user=>!!user));
+      return this.authService.user.pipe(filter(user=>typeof user !== 'undefined'), map(user=>!user));
   }
   
 }
