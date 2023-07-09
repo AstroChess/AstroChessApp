be83b31205faafe3c4d117b9365ec2c0bb3d543e
xketris <xketris@gmail.com>
2023-07-09 23:37:24 +0200

    app-routing child routes, homepage path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,33 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileGuard } from './profile/profile.guard';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomepageComponent } from './homepage/homepage.component';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
   {
-    path: '', component: AppComponent, pathMatch: 'full'
+    path: '', component: AppComponent, children: [
+      {
+        path: 'home', component: HomepageComponent
+      },
+      {
+        path: 'auth', loadChildren: ()=>import('./auth/auth.module').then(m=>m.AuthModule)
+      },
+      {
+        path: 'profile', component: ProfileComponent, canActivate: [ProfileGuard]
+      },
+      {
+        path: 'game', loadChildren: ()=>import('./chess/chess.module').then(m=>m.ChessModule), canActivate: [ProfileGuard]
+      },
+      {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+      },
+      {
+        path: '**', component: PageNotFoundComponent
+      }
+    ]
   },
-  {
-    path: 'auth', loadChildren: ()=>import('./auth/auth.module').then(m=>m.AuthModule)
-  },
-  {
-    path: 'profile', component: ProfileComponent, canActivate: [ProfileGuard]
-  },
-  {
-    path: 'game', loadChildren: ()=>import('./chess/chess.module').then(m=>m.ChessModule), canActivate: [ProfileGuard]
-  },
-  {
-    path: '**', component: PageNotFoundComponent
-  }
+  
 ];
 
 @NgModule({
