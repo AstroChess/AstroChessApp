b94dad15fda82d8343081755527c0120bc8debf3
xketris <xketris@gmail.com>
2023-07-12 14:26:53 +0200

    moving pieces

--- src/app/chess/board/board.component.ts
+++ src/app/chess/board/board.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  selectedRow: number | null = null;
+  selectedColumn: number | null = null;
   board = [
     ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
     ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
@@ -20,4 +22,23 @@ export class BoardComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  handlePositionChange(row: number, column:number) {
+    if(!this.selectedColumn && !this.selectedRow) {
+      this.setPositions(row, column);
+    } else {
+      this.changePositions(row, column);
+    }
+  }
+
+  private setPositions(row: number, column:number) {
+    this.selectedColumn = column;
+    this.selectedRow = row;
+  }
+
+  private changePositions(row: number, column:number) {
+    this.board[row][column] = this.board[this.selectedRow!].splice(this.selectedColumn!, 1, this.board[row][column])[0];
+    this.selectedColumn = null;
+    this.selectedRow = null;
+  }
 }
