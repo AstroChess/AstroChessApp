067d9ff64be0610ad778045114b7ae314e20dbc3
xketris <xketris@gmail.com>
2023-07-13 12:13:21 +0200

    chess.js board implementation

--- src/app/chess/board/board.component.ts
+++ src/app/chess/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chess, Square, PieceSymbol, Color } from 'chess.js';
 
 @Component({
   selector: 'app-board',
@@ -8,38 +9,36 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   selectedRow: number | null = null;
   selectedColumn: number | null = null;
-  board = [
-    ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
-    ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', ''],
-    ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
-    ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
-  ];
+  chessInstance: Chess = new Chess();
+  board!: ({ square: Square; type: PieceSymbol; color: Color } | null)[][];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.board = this.chessInstance.board();
+  }
 
-  handlePositionChange(row: number, column:number) {
-    if(!this.selectedColumn && !this.selectedRow && this.board[row][column]) {
+  handlePositionChange(row: number, column: number) {
+    if (!this.selectedColumn && !this.selectedRow && this.board[row][column]) {
       this.setPositions(row, column);
-    } else if(this.selectedColumn!==null && this.selectedRow!==null) {
+    } else if (this.selectedColumn !== null && this.selectedRow !== null) {
       this.changePositions(row, column);
     } else {
       this.clearSelectedFields();
     }
   }
 
-  private setPositions(row: number, column:number) {
+  private setPositions(row: number, column: number) {
     this.selectedColumn = column;
     this.selectedRow = row;
   }
 
-  private changePositions(row: number, column:number) {
-    this.board[row][column] = this.board[this.selectedRow!].splice(this.selectedColumn!, 1, this.board[row][column])[0];
+  private changePositions(row: number, column: number) {
+    this.board[row][column] = this.board[this.selectedRow!].splice(
+      this.selectedColumn!,
+      1,
+      this.board[row][column]
+    )[0];
     this.clearSelectedFields();
   }
 
