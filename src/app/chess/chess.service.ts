b5da2dbfc419e36ec7884b0327bb981f7f004991
xketris <xketris@gmail.com>
2023-07-20 20:13:18 +0200

    creating new game

--- src/app/chess/chess.service.ts
+++ src/app/chess/chess.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 
 import { BehaviorSubject } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChessService {
+  supabase: SupabaseClient;
   whoseMove = new BehaviorSubject<'w' | 'b' | 'finished'>('w');
 
-  constructor() {
+  constructor(private authService: AuthService) {
+    this.supabase = createClient(env.supabaseUrl, env.supabaseApi);
   }
 
   playAudio(type: 'check' | 'move' | 'notify') {
@@ -16,5 +22,14 @@ export class ChessService {
     audio.src = `/assets/sound/${type}.mp3`
     audio.load();
     audio.play();
+  }
+
+  async createGame(minutes: number) {
+    const playersColor = ['white', 'black'][Math.floor(Math.random() * 2)];
+    const data = {
+      [playersColor+'_player']: this.authService.user.value.id,
+      'minutes_per_player': minutes
+    }
+    return await this.authService.supabase.from('games').insert(data);
   }
 }
