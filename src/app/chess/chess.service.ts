8741eb298cd2101c03bd826f2910dda452075e45
xketris <xketris@gmail.com>
2023-07-22 16:27:01 +0200

    players info, code cleanup/migration to service

--- src/app/chess/chess.service.ts
+++ src/app/chess/chess.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 
 import { BehaviorSubject } from 'rxjs';
@@ -12,8 +13,11 @@ import { AuthService } from '../auth/auth.service';
 export class ChessService {
   supabase: SupabaseClient;
   whoseMove = new BehaviorSubject<'w' | 'b' | 'finished'>('w');
+  playerColor!: 'w' | 'b';
+  opponent!: any
 
-  constructor(private authService: AuthService) {
+
+  constructor(private authService: AuthService, private router: Router) {
     this.supabase = createClient(env.supabaseUrl, env.supabaseApi);
   }
 
@@ -31,5 +35,64 @@ export class ChessService {
       'minutes_per_player': minutes
     }
     return await this.authService.supabase.from('games').insert(insertData).select('*');
+  }
+
+  async findGame(minutesPerPlayer: number) {
+    const { data: games } = await this.supabase
+      .from('games')
+      .select('*')
+      .eq('minutes_per_player', minutesPerPlayer)
+      .or(
+        `and(black_player.neq.${this.authService.user.value.id}, white_player.is.null), and(black_player.is.null, and(white_player.neq.${this.authService.user.value.id}))`
+      );
+
+    if (games && games.length > 0) {
+      const randomIndex = Math.floor(Math.random() * games.length);
+      const chosenGame = games[randomIndex];
+      const whichPlayer = chosenGame['white_player']
+        ? 'black_player'
+        : 'white_player';
+      const gameId = chosenGame['game_id'];
+
+      this.playerColor = whichPlayer==='white_player' ? 'w' : 'b';
+      this.opponent = chosenGame['white_player'] || chosenGame['black_player'];
+      
+      await this.supabase
+        .from('games')
+        .update({ [whichPlayer]: this.authService.user.value.id })
+        .eq('game_id', gameId);
+
+      this.router.navigate(['game', gameId]);
+      return;
+    }
+
+    const game = await this.createGame(minutesPerPlayer);
+
+    if (game.error) {
+      console.log('Some error occurred');
+      return;
+    }
+
+    const gameId = game.data[0]['game_id'];
+    const color = game.data[0]['white_player'] ? 'w' : 'b';
+
+    this.supabase
+      .channel('schema-db-changes')
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'games',
+          filter: `game_id=eq.${gameId}`,
+        },
+        (payload: any) => {
+          this.playerColor = color;
+          this.opponent = payload.new[color==='w' ? 'black_player' : 'white_player'];
+          this.playAudio('notify');
+          this.router.navigate(['game', gameId]);
+        }
+      )
+      .subscribe();
   }
 }
--- src/app/chess/new-game/new-game.component.ts
+++ src/app/chess/new-game/new-game.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
 import { ChessService } from '../chess.service';
-import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-new-game',
@@ -11,63 +9,10 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class NewGameComponent {
   constructor(
-    private chessService: ChessService,
-    private authService: AuthService,
-    private router: Router,
-    private route: ActivatedRoute
+    private chessService: ChessService
   ) {}
 
   async findGame(minutesPerPlayer: number) {
-    const { data: games } = await this.chessService.supabase
-      .from('games')
-      .select('*')
-      .eq('minutes_per_player', minutesPerPlayer)
-      .or(
-        `and(black_player.neq.${this.authService.user.value.id}, white_player.is.null), and(black_player.is.null, and(white_player.neq.${this.authService.user.value.id}))`
-      );
-
-    if (games && games.length > 0) {
-      const randomIndex = Math.floor(Math.random() * games.length);
-      const chosenGame = games[randomIndex];
-      const whichPlayer = games[randomIndex]['white_player']
-        ? 'black_player'
-        : 'white_player';
-      const gameId = chosenGame['game_id'];
-      console.log(whichPlayer);
-      await this.chessService.supabase
-        .from('games')
-        .update({ [whichPlayer]: this.authService.user.value.id })
-        .eq('game_id', gameId);
-
-      this.router.navigate([gameId], { relativeTo: this.route });
-      return;
-    }
-
-    const game = await this.chessService.createGame(minutesPerPlayer);
-
-    if (game.error) {
-      console.log('Some error occurred');
-      return;
-    }
-
-    const gameId = game.data[0]['game_id'];
-
-    this.chessService.supabase
-      .channel('schema-db-changes')
-      .on(
-        'postgres_changes',
-        {
-          event: 'UPDATE',
-          schema: 'public',
-          table: 'games',
-          filter: `game_id=eq.${gameId}`,
-        },
-        (payload: any) => {
-          console.log('payload!!', payload);
-          this.chessService.playAudio('notify');
-          this.router.navigate([gameId], { relativeTo: this.route });
-        }
-      )
-      .subscribe();
+    await this.chessService.findGame(minutesPerPlayer);
   }
 }
