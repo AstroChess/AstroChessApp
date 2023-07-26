0b31e667398328971d91688321fd597d10a91b00
xketris <xketris@gmail.com>
2023-07-26 19:02:58 +0200

    code cleanup

--- src/app/chess/game/game.service.ts
+++ src/app/chess/game/game.service.ts
@@ -14,32 +14,7 @@ export class GameService {
     player!: any;
     opponent!: any;
 
-    constructor(private authService: AuthService, private router: Router) {}
-
-    async storeInfo(gameId: string) {
-        const userId = this.authService.user.value?.id;
-        const username = this.authService.user.value?.user_metadata['username'];
-        let gameData;
-
-        if(localStorage.getItem('gameData')) {
-            gameData = JSON.parse(localStorage.getItem('gameData')!);
-        } else {
-            gameData = await this.fetchGameData(gameId);
-            localStorage.setItem('gameData', JSON.stringify(gameData));
-        }
-        
-        if(gameData.error) {
-            this.router.navigateByUrl('');
-            return;
-        }
-        gameData = gameData.data;
-        const isPlayerWhite = gameData['white_player']===userId;
-        const opponentId = isPlayerWhite ? gameData['black_player'] : gameData['white_player'];
-        const playerColor = isPlayerWhite ? 'w' : 'b';
-        this.gameData = gameData;
-        
-        return { username, opponentId, playerColor };
-    }
+    constructor(private authService: AuthService) {}
 
     async fetchGameData(gameId: string) {
         const result = await this.authService.supabase.from('games').select('*, white_player(userid, username), black_player(userid, username)').eq('game_id', gameId).single();
