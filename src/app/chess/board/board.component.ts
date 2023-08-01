6716605e89dc36708a44c605767c25c33607229d
xketris <xketris@gmail.com>
2023-08-01 02:59:56 +0200

    fetching data after moves

--- src/app/chess/board/board.component.ts
+++ src/app/chess/board/board.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
 
 import { filter, take } from 'rxjs';
 import { Chess, Square, PieceSymbol, Color } from 'chess.js';
@@ -10,7 +15,7 @@ import { GameService } from '../game/game.service';
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BoardComponent implements OnInit {
   @Input() color!: 'w' | 'b';
@@ -20,59 +25,86 @@ export class BoardComponent implements OnInit {
   chessInstance: Chess = new Chess();
   board!: ({ square: Square; type: PieceSymbol; color: Color } | null)[][];
   possibleMoves: (string | undefined)[] = [];
-  lastMoves: {from: string, to: string}[]= [];
-  lastMove: {from: string, to: string} | undefined;
+  lastMoves: { from: string; to: string }[] = [];
+  lastMove: { from: string; to: string } | undefined;
 
-  constructor(private gameService: GameService, private chessService: ChessService) {}
+  constructor(
+    private gameService: GameService,
+    private chessService: ChessService
+  ) {}
 
   ngOnInit(): void {
     this.reloadBoard();
-    this.gameService.whoseMove.pipe(filter(val=>val==='finished'), take(1)).subscribe(
-      ()=>this.chessService.playAudio('notify')
-    )
+    this.gameService.whoseMove
+      .pipe(
+        filter((val) => val === 'finished'),
+        take(1)
+      )
+      .subscribe(() => this.chessService.playAudio('notify'));
+    this.chessService.supabase
+      .channel('schema-db-changes')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'moves',
+          filter: `game_id=eq.${this.gameService.gameData.game_id}`,
+        },
+        (payload) => {
+          if (payload.new['color'] !== this.color)
+            console.log('response', payload);
+        }
+      )
+      .subscribe();
   }
 
-  async handlePositionChange(row: number, column: number, field: { square: Square; type: PieceSymbol; color: Color } | null) {
-    if (this.gameService.whoseMove.value==='finished' || (field && this.color!==field.color) || this.gameService.whoseMove.value!==this.color) {
+  handlePositionChange(
+    row: number,
+    column: number,
+    field: { square: Square; type: PieceSymbol; color: Color } | null,
+    mode: 'player' | 'opponent'
+  ) {
+    const whoseMove = this.gameService.whoseMove.value;
+    if (
+      whoseMove === 'finished' ||
+      (field && this.color !== field.color) ||
+      (whoseMove !== this.color && mode === 'player')
+    ) {
       return;
     }
 
     if (!this.selectedColumn && !this.selectedRow && this.board[row][column]) {
-      if(field?.color===this.gameService.whoseMove.value) {
+      if (field?.color === this.gameService.whoseMove.value) {
         this.highlightPossibleMoves(field.square);
         this.setPositions(row, column);
       }
-    } else if (this.selectedColumn !== null && this.selectedRow !== null && !(row===this.selectedRow && column===this.selectedColumn)) {
-      if(field?.color===this.board[this.selectedRow][this.selectedColumn]?.color) {
+    } else if (
+      this.selectedColumn !== null &&
+      this.selectedRow !== null &&
+      !(row === this.selectedRow && column === this.selectedColumn)
+    ) {
+      if (
+        field?.color ===
+        this.board[this.selectedRow][this.selectedColumn]?.color
+      ) {
         this.highlightPossibleMoves(field?.square!);
         this.setPositions(row, column);
       } else {
         this.changePositions(row, column);
-        const data = {
-          game_id: this.gameService.gameData.game_id,
-          user_id: this.gameService.player.userid,
-          color: this.color==='w' ? 'white' : 'black',
-          from: this.getSquare(this.selectedRow, this.selectedColumn),
-          to: this.getSquare(row, column),
-          FEN_after: this.chessInstance.fen(),
-          remaining_time_ms: this.gameService.timeToEnd
-        };
-        console.log(data)
-        const selected = await this.chessService.supabase.from('moves').insert(data).select('*');
-        console.log(selected);
       }
     } else {
       this.clearSelectedFields();
       this.clearPossibleMoves();
     }
-    
+
     if (this.chessInstance.isGameOver()) {
       this.gameService.whoseMove.next('finished');
     }
   }
 
   getSquare(row: number, column: number) {
-    return `${String.fromCharCode(97+column)}${8-row}`;
+    return `${String.fromCharCode(97 + column)}${8 - row}`;
   }
 
   private setPositions(row: number, column: number) {
@@ -80,7 +112,7 @@ export class BoardComponent implements OnInit {
     this.selectedRow = row;
   }
 
-  private changePositions(row: number, column: number) {
+  private async changePositions(row: number, column: number) {
     const fromField = this.getSquare(this.selectedRow!, this.selectedColumn!);
     const toField = this.getSquare(row, column);
 
@@ -89,7 +121,7 @@ export class BoardComponent implements OnInit {
       to: toField,
     });
 
-    this.lastMoves.push({from: fromField, to: toField}); 
+    this.lastMoves.push({ from: fromField, to: toField });
     this.highlightLastMove(fromField, toField);
 
     this.chessInstance.load(move.after);
@@ -97,14 +129,29 @@ export class BoardComponent implements OnInit {
 
     this.chessService.playAudio('move');
 
-    console.log(move);
-    this.onWhoseMoveChange();
+    const data = {
+      game_id: this.gameService.gameData.game_id,
+      user_id: this.gameService.player.userid,
+      color: this.color === 'w' ? 'white' : 'black',
+      from: move.from,
+      to: move.to,
+      FEN_after: move.after,
+      remaining_time_ms: this.gameService.timeToEnd,
+    };
+    console.log(data);
     this.clearSelectedFields();
     this.clearPossibleMoves();
+    if(this.gameService.whoseMove.value===this.color) {
+      await this.chessService.supabase.from('moves').insert(data);
+    }
+    this.onWhoseMoveChange();
   }
 
   private reloadBoard() {
-    this.board = this.color==='w' ? this.chessInstance.board() : this.chessInstance.board().reverse();
+    this.board =
+      this.color === 'w'
+        ? this.chessInstance.board()
+        : this.chessInstance.board().reverse();
   }
 
   private clearSelectedFields() {
@@ -113,15 +160,19 @@ export class BoardComponent implements OnInit {
   }
 
   private onWhoseMoveChange() {
-    this.gameService.whoseMove.next(this.gameService.whoseMove.value==='w' ? 'b' : 'w');
+    this.gameService.whoseMove.next(
+      this.gameService.whoseMove.value === 'w' ? 'b' : 'w'
+    );
   }
 
   private highlightPossibleMoves(square: Square) {
-    this.possibleMoves = this.chessInstance.moves({square: square}).map(val=>val.match(/[a-z]{1}[1-8]{1}/)?.join(''));
+    this.possibleMoves = this.chessInstance
+      .moves({ square: square })
+      .map((val) => val.match(/[a-z]{1}[1-8]{1}/)?.join(''));
   }
 
   private highlightLastMove(from: string, to: string) {
-    this.lastMove = {from, to};
+    this.lastMove = { from, to };
   }
 
   private clearPossibleMoves() {
