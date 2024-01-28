89703a122a6e348aeacb4940154089c722ca481e
szymon-skalmierski <xketris@gmail.com>
2024-01-28 17:24:12 +0100

    config

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -50,6 +50,3 @@ import 'zone.js'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-import * as process from 'process';
-window['process'] = process;
-console.log(process);
