28e4ba91930c0e5f719b268838e8b3af3896b685
szymon-skalmierski <xketris@gmail.com>
2024-01-29 13:25:21 +0100

    setting env variables

---
+++ src/environments/set-env.js
@@ -0,0 +1,31 @@
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+// Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+// Load node modules
+  const colors = require('colors');
+  const appVersion = require('../../package.json').version;
+  require('dotenv').config({
+    path: './.env'
+  });
+// `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+  googleApiKey: '${process.env.GOOGLE_API_KEY}',
+  auth0ClientId: '${process.env.AUTH0_CLIENT_ID}',
+  appVersion: '${appVersion}',
+  production: true,
+};
+`;
+  console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+  writeFile(targetPath, envConfigFile, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+    }
+  });
+};
+
+setEnv();
