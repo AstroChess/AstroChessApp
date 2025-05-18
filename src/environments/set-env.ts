811b94b2603081bbb9696f74a3fd24718f780fa4
szymon-skalmierski <xketris@gmail.com>
2025-05-18 16:41:25 +0200

    set-env.ts cleaning

--- src/environments/set-env.ts
+++ src/environments/set-env.ts
@@ -1,29 +1,22 @@
-const setEnv = () => {
+(() => {
   const fs = require('fs');
   const writeFile = fs.writeFile;
-// Configure Angular `environment.ts` file path
   const targetPath = './src/environments/environment.prod.ts';
-// Load node modules
-  const appVersion = require('../../package.json').version;
+
   require('dotenv').config({
-    path: 'src/environments/.env'
+    path: 'src/environments/.env',
   });
-// `environment.ts` file structure
+
   const envConfigFile = `export const environment = {
-  SUPABASE_URL: '${process.env.SUPABASE_URL}',
-  SUPABASE_API: '${process.env.SUPABASE_API}',
-  production: true,
-};
-`;
-console.log('The file `environment.ts` will be written with the following content:');
+    SUPABASE_URL: '${process.env.SUPABASE_URL}',
+    SUPABASE_API: '${process.env.SUPABASE_API}',
+    production: true,
+  };`;
+
   writeFile(targetPath, envConfigFile, (err: any) => {
     if (err) {
       console.error(err);
       throw err;
-    } else {
-      console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
     }
   });
-};
-
-setEnv();
+})();
